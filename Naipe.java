ef20d761b0faeb7fff264d036b8cb953242c2559
Kyousukee1 <king-q@hotmail.com.br>
2016-09-24 12:46:56 -0300

    Add files via upload

---
+++ Naipe.java
@@ -0,0 +1,19 @@
+package blackjack;
+
+public class Naipe {
+
+	public Naipe(String nome) {
+		this.nome = nome;
+	}
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
