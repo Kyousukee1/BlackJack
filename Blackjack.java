52eb449d86393a60e3a488f5826fdad73c7940ca
Kyousukee1 <king-q@hotmail.com.br>
2016-09-29 23:50:13 -0300

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -3,15 +3,7 @@ package blackjack;
 public class Blackjack {
 
 	public static void main(String[] args) {
-		
-		int carta;
-		
-		Baralho baralho = new Baralho();
-		Banca banca = new Banca();
-		Cliente cliente = new Cliente();
-		
-		carta = Baralho.getCartaAleatoria();
-		System.out.println(carta);
+
 	}
 
 }
